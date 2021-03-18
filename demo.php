c6642aa7d3c78c262c0ba9eb850258c5cec04d4d
deepthimiryala <80376296+deepthimiryala@users.noreply.github.com>
2021-03-17 23:08:12 -0700

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+$firstname=$_POST["firstname"];
+$Surname=$_POST["Surname"];
+$Mobilenumber=$_POST["mobilenum"];
+$Emailid=$_POST["emailid"];
+$Newpassword=$_POST["Newpassword"];
+echo "welcome genius".$firstname;
+?>
